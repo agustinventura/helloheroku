b8c1b4a76276beae93be5d2f56ae28d78d94c78c
Agustín Ventura Carrasco <agustinventura@gmail.com>
2011-09-22 20:05:49 +0200

    añadido Redis para caché de lectura

--- src/main/java/es/aguasnegras/helloheroku/TickDAO.java
+++ src/main/java/es/aguasnegras/helloheroku/TickDAO.java
@@ -1,19 +1,49 @@
 package es.aguasnegras.helloheroku;
 
 import java.sql.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.pool.impl.GenericObjectPool.Config;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Protocol;
+
 
 public class TickDAO {
 	private static String dbUrl;
-	
+	private static JedisPool jedisPool;
+	private static final String TICKCOUNT_KEY = "tickcount";
+
 	static {
 		dbUrl = System.getenv("DATABASE_URL");
 		dbUrl = dbUrl.replaceAll("postgres://(.*):(.*)@(.*)", "jdbc:postgresql://$3?user=$1&password=$2");
 		System.out.println("the jdbc connection string is: " + dbUrl);
+		//Inicialización de Redis
+		Pattern REDIS_URL_PATTERN = Pattern.compile("^redis://([^:]*):([^@]*)@([^:]*):([^/]*)(/)?");
+		Matcher matcher = REDIS_URL_PATTERN.matcher(System.getenv("REDISTOGO_URL"));
+		matcher.matches();
+		Config config = new Config();
+		config.testOnBorrow = true;
+		jedisPool = new JedisPool(config, matcher.group(3),
+		Integer.parseInt(matcher.group(4)), Protocol.DEFAULT_TIMEOUT, matcher.group(2));
+
 	}
 
 	public int getTickCount() throws SQLException {
-		return getTickcountFromDb();
+		Jedis jedis = jedisPool.getResource();
+		int tickcount = 0;
+		String tickcountValue = jedis.get(TICKCOUNT_KEY);
+		if (tickcountValue != null) {
+			System.out.println("read from redis cache");
+			tickcount = Integer.parseInt(tickcountValue);
+		} else {
+			tickcount = getTickcountFromDb();
+			jedis.setex(TICKCOUNT_KEY, 30, String.valueOf(tickcount));
+		}
+		jedisPool.returnResource(jedis);
+		return tickcount;
 	}
+
 
 	public static int getScalarValue(String sql) throws SQLException {
 		Connection dbConn = null;
